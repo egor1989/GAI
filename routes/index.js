9b8716b4a4cf8953c1f0e56e33bd2c04472254cd
Pavel Ershov <owner.mad.epa@gmail.com>
2012-04-22 02:31:26 +0400

    Another unused code

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-var db = require("../src/shipment/db/default-schema").Shipment;
+var db = require("../src/rtc/db/default-schema").RTC;
 
 /*
  * GET home page.
@@ -7,33 +7,6 @@ var db = require("../src/shipment/db/default-schema").Shipment;
 exports.index = function(req, res){
     res.render('index', {
       login: req.cookies.login,
-        title: 'TX'
+        title: 'ДТП'
     });
-};
-
-exports.countries = function(req, res){
-    db.getCountries(function(err, data) {
-        res.render('countries', {
-            login: req.cookies.login,
-            title: 'Shipping tx',
-            countries: data
-        });
-    });
-};
-
-exports.shipping = function(req, res){
-    var country = req.query.country;
-
-    res.render('shipping', {
-        login: req.cookies.login,
-        title: req.query.country,
-        country: req.query.country,
-    });
-};
-
-exports.atm = function(req, res){
-  res.render('atm', {
-      title: 'ATM tx',
-      login: req.cookies.login
-  });
 };
