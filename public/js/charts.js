388c1c06e56605824202eda7099ef72a25288f8d
efremov <efremovvb@gmail.com>
2012-10-07 15:30:09 +0400

    добавлены пироги, использованы данные за 12й год, добавлен выбор чарта по клику

--- public/js/charts.js
+++ public/js/charts.js
@@ -1,22 +1,39 @@
-function createChart(chart, series) {
+function createChart(chart, title, series) {
     $('#' + chart).addClass('chart');
+
     return new Highcharts.Chart({
-        chart: {
-            renderTo: chart,
-            type: 'line'
-        },
-        title: {
-            text: 'Fruit Consumption'
-        },
-        xAxis: {
-            categories: ['Apples', 'Bananas', 'Oranges']
-        },
-        yAxis: {
-            title: {
-                text: 'Fruit eaten'
+        chart:{
+            renderTo:chart,
+            type:'pie',
+            events:{
+                click:function (event, data) {
+                    var $container = $(this.container).parent();
+                    $container.parent().trigger('chartSelected', {chart:this, $container: $container });
+                }
             }
         },
-        series: series
+        title:{
+            text:title
+        },
+        plotOptions:{
+            pie:{
+
+                cursor:'pointer',
+                dataLabels:{
+                    enabled:true,
+                    color:'#000000',
+                    connectorColor:'#000000',
+                    formatter:function () {
+                        return '<b>' + this.point.name + '</b>: ' + Math.floor(this.percentage) + ' %';
+                    }
+                }
+            }
+        },
+        tooltip:{
+            pointFormat:'<b>{point.y}</b> ({point.percentage}%)',
+            percentageDecimals:1
+        },
+        series:series
     });
 }
 
